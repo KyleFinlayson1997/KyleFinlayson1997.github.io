1962dd2ece03da7d7fbc6da7ae45002d7d14dbf6
Kyle <kyle.finlayson1997@gmail.com>
2023-03-15 22:23:56 +1000

    Initial commit

---
+++ calc.js
@@ -0,0 +1,32 @@
+function setup() {
+    var userInput = document.getElementById('userInput');
+    var resultLabel = document.getElementById('resultLabel');
+
+    userInput.addEventListener('input', function () {
+        var daysPassed = userInput.valueAsNumber;
+        if (isNaN(daysPassed)) {
+            resultLabel.innerHTML = '...';
+        }
+        else {
+            resultLabel.innerHTML = returnString(daysPassed);
+        }
+    });
+
+    function returnString(x) {
+        var MESSAGE;
+        var dayIncrementCount = 0;
+        if ((x % 8) == 0) {
+            MESSAGE = `A Necromancer's Moon is already occurring on day <span>${x}</span>.`;
+        }
+        else {
+            while ((x % 8) != 0) {
+                dayIncrementCount += 1;
+                x += 1;
+            }
+            MESSAGE = `A Necromancer's Moon will occur on day <span>${x}</span> in <span>${dayIncrementCount}</span> days.`;
+        }
+        return MESSAGE;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', setup);
