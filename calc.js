6899b2d3d8b79ae10834f0d179dd686afa997270
Kyle <kyle.finlayson1997@gmail.com>
2023-03-16 07:47:28 +1000

    refactored css file, cleaned up

--- calc.js
+++ calc.js
@@ -16,14 +16,14 @@ function setup() {
         var MESSAGE;
         var dayIncrementCount = 0;
         if ((x % 8) == 0) {
-            MESSAGE = `A Necromancer's Moon is already occurring on day <span>${x}</span>.`;
+            MESSAGE = `A Necromancer's Moon is already occurring on day <span class="highlight">${x}</span>.`;
         }
         else {
             while ((x % 8) != 0) {
                 dayIncrementCount += 1;
                 x += 1;
             }
-            MESSAGE = `A Necromancer's Moon will occur on day <span>${x}</span> in <span>${dayIncrementCount}</span> days.`;
+            MESSAGE = `A Necromancer's Moon will occur on day <span class="highlight">${x}</span> in <span class="highlight">${dayIncrementCount}</span> days.`;
         }
         return MESSAGE;
     }
