366ebe579df3652b55f4c9bda12638bb6dce943e
Kyle <kyle.finlayson1997@gmail.com>
2023-03-16 16:29:37 +1000

    fixed naming to be in-line with wiki pages

--- calc.js
+++ calc.js
@@ -19,14 +19,14 @@ function setup() {
         var MESSAGE;
         var dayIncrementCount = 0;
         if ((x % 8) == 0) {
-            MESSAGE = `A Necromancer's Moon is already occurring on day <span class="highlight">${x}</span>.`;
+            MESSAGE = `The Shade of the Revenant is already occurring on day <span class="highlight">${x}</span>.`;
         }
         else {
             while ((x % 8) != 0) {
                 dayIncrementCount += 1;
                 x += 1;
             }
-            MESSAGE = `A Necromancer's Moon will occur on day <span class="highlight">${x}</span> in <span class="highlight">${dayIncrementCount}</span> days.`;
+            MESSAGE = `The next Shade of the Revenant will occur on day <span class="highlight">${x}</span> in <span class="highlight">${dayIncrementCount}</span> days.`;
         }
         return MESSAGE;
     }
